0d8a6b3c881a2059167227d05c73b16c50c56bf4
raheemadamboev <raheemideadev@gmail.com>
2022-02-01 17:53:13 +0500

    Arrays

--- src/TheArrays.java
+++ src/TheArrays.java
@@ -3,6 +3,27 @@ import java.util.Arrays;
 public class TheArrays {
 
     public static void main(String[] args) {
+        String[] footballers = new String[5];
+        footballers[0] = "Ronaldo";
+        footballers[1] = "Messi";
+        footballers[4] = "Lewandowski";
 
+        System.out.println(Arrays.toString(footballers));
+
+        int[] goals = {900, 800, 500};
+
+        for (int i = 0; i < goals.length; i++) {
+            System.out.println(goals[i]);
+        }
+
+        for (int i = footballers.length - 1; i >= 0; i--) {
+            System.out.println(footballers[i]);
+        }
+
+        for (String footballer : footballers) {
+            System.out.println(footballer);
+        }
+
+        Arrays.stream(footballers).forEach(System.out::println);
     }
 }
