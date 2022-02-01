a06da148d8686b9d3a81cb065edce487c935e48b
raheemadamboev <raheemideadev@gmail.com>
2022-02-01 18:22:00 +0500

    Two dimensional arrays

---
+++ src/TheTwoDimensionalArrays.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class TheTwoDimensionalArrays {
+
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        board[0][0] = 'o';
+        board[1][0] = 'o';
+        board[2][0] = 'o';
+
+        System.out.println(Arrays.deepToString(board));
+
+        char[][] board2 = new char[][]{
+                new char[]{'o', '-', '-'},
+                new char[]{'o', '-', '-'},
+                new char[]{'o', '-', '-'}
+        };
+
+        System.out.println(Arrays.deepToString(board2));
+    }
+}
